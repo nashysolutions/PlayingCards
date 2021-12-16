13908f8b83b3d3d53b2ecc85a43fb8140da2f8d0
Rob Nash <r.nash1@me.com>
2021-12-16 05:42:48 +0000

    Provide symbol representations.

--- Sources/PlayingCards/Suit.swift
+++ Sources/PlayingCards/Suit.swift
@@ -16,3 +16,42 @@ extension Suit {
     }
 }
 
+extension Suit {
+    
+    public func symbol(style: SymbolStyle = .hallow) -> String {
+        switch self {
+        case .spades: return spades(style)
+        case .hearts: return hearts(style)
+        case .diamonds: return diamonds(style)
+        case .clubs: return clubs(style)
+        }
+    }
+
+    private func spades(_ style: SymbolStyle) -> String {
+        switch style {
+        case .filled: return "♠"
+        case .hallow: return "♤"
+        }
+    }
+
+    private func hearts(_ style: SymbolStyle) -> String {
+        switch style {
+        case .filled: return "♥"
+        case .hallow: return "♡"
+        }
+    }
+
+    private func diamonds(_ style: SymbolStyle) -> String {
+        switch style {
+        case .filled: return "♦"
+        case .hallow: return "♢"
+        }
+    }
+
+    private func clubs(_ style: SymbolStyle) -> String {
+        switch style {
+        case .filled: return "♣"
+        case .hallow: return "♧"
+        }
+    }
+}
